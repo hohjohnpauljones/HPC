73d9e3a6c38e70639113dca785e8c3c045d0e9c0
EC2 Default User <ec2-user@ip-172-31-38-211.us-west-2.compute.internal>
2014-05-07 23:27:56 +0000

    i think i rotated to right 90 degrees

--- hw6/hw6_sp14/HW6.cu
+++ hw6/hw6_sp14/HW6.cu
@@ -11,7 +11,7 @@ __global__ void kernel( uint8_t *d_input, uint8_t *d_output ) {
 	int x = blockIdx.x;
 	int y = blockIdx.y;
 	int offset = x + y * gridDim.x;
-	int offset2 = x + (gridDim.y - y - 1);
+	int offset2 = gridDim.y*(x+1) - y - 1;
 	d_output[offset] = d_input[offset2];
 	
 }
