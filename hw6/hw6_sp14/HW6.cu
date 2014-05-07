db2d92ea068dda6fdce39166ec29ff492fda2a71
gaatyd_112113 <gaatyd@babbage2.cs.missouri.edu>
2014-05-07 18:59:40 -0500

    added some quicksort stuff

--- hw6/hw6_sp14/HW6.cu
+++ hw6/hw6_sp14/HW6.cu
@@ -6,6 +6,71 @@
 
 typedef unsigned char uint8_t;
 
+/* This function swaps two numbers
+   Arguments :
+			 a, b - the numbers to be swapped
+   */
+void swap(int &a, int &b)
+{
+	int temp;
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+/* This function does the quicksort
+   Arguments :
+			 array - the array to be sorted
+			 startIndex - index of the first element of the section
+			 endIndex - index of the last element of the section
+   */
+void QuickSort(uint8_t* array, int startIndex, int endIndex)
+{
+	int pivot = array[startIndex];	//pivot element is the leftmost element
+	int splitPoint;
+	
+	if(endIndex > startIndex)
+	{
+		splitPoint = SplitArray(array, pivot, startIndex, endIndex);
+		array[splitPoint] = pivot;
+		QuickSort(array, startIndex, splitPoint-1);   //Quick sort first half
+		QuickSort(array, splitPoint+1, endIndex);	 //Quick sort second half
+	}
+}
+
+/* This function splits the array around the pivot
+   Arguments :
+			 array - the array to be split
+			 pivot - pivot element whose position will be returned
+			 startIndex - index of the first element of the section
+			 endIndex - index of the last element of the section
+   Returns :
+		   the position of the pivot
+   */
+int SplitArray(uint8_t* array, int pivot, int startIndex, int endIndex)
+{
+	int leftBoundary = startIndex;
+	int rightBoundary = endIndex;
+	
+	while(leftBoundary < rightBoundary)			   //shuttle pivot until the boundaries meet
+	{
+		 while( pivot < array[rightBoundary]		  //keep moving until a lesser element is found
+				&& rightBoundary > leftBoundary)	  //or until the leftBoundary is reached
+		 {
+			  rightBoundary--;						//move left
+		 }
+		 swap(array[leftBoundary], array[rightBoundary]);
+		 
+		 while( pivot >= array[leftBoundary]		  //keep moving until a greater or equal element is found
+				&& leftBoundary < rightBoundary)	  //or until the rightBoundary is reached
+		 {
+			  leftBoundary++;						 //move right
+		 }
+		 swap(array[rightBoundary], array[leftBoundary]);
+	}
+	return leftBoundary;
+}
+
 __global__ void kernel( uint8_t *d_input, uint8_t *d_output ) {
 	// map from threadIdx/BlockIdx to pixel position
 	int x = blockIdx.x;
